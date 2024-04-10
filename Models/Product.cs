26874addd6733d92148377a3c35287f72d9da389
Karamjeet Uppal <148819621+kuppal13@users.noreply.github.com>
2024-04-10 00:39:30 -0400

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.WPF.ADO.ConnectionModels.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double ProductPrice { get; set; }
+        public string ProductColor { get; set; }
+        public User Users { get; set; }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.WPF.ADO.ConnectionModels.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserRole { get; set; }
+        //public string UserDetail { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
